1de92b3c806f2e2aed61c9ec48315b3e3ad1c511
Edward Tanguay (windows) <edward@tanguay.info>
2022-08-22 10:03:33 +0200

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.scss';
+
+function App() {
+		return (
+				<div className="App">
+						<h1>Language Tandem Group</h1>
+						<p>testing</p>
+				</div>
+		);
+}
+
+export default App;
