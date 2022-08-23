96d438214054105f3d8c792b9ffbaebe0a9c6807
Edward Tanguay (windows) <edward@tanguay.info>
2022-08-23 11:18:58 +0200

    two pages protected

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
 		username: '',
 		firstName: '',
 		lastName: '',
-		accessGroups: []
+		accessGroups: [],
 	});
 
 	useEffect(() => {
@@ -31,21 +31,28 @@ function App() {
 	return (
 		<div className="App">
 			<h1>Language Tandem Group</h1>
-			<h1>{currentUser.username}</h1>
 			<nav>
 				<NavLink to="/welcome">Welcome</NavLink>
-				<NavLink to="/members">Members</NavLink>
+				{currentUser.accessGroups.includes('members') && (
+					<NavLink to="/members">Members</NavLink>
+				)}
 				<NavLink to="/register">Register</NavLink>
 				<NavLink to="/login">Login</NavLink>
-				<NavLink to="/logout">Logout</NavLink>
+				{currentUser.accessGroups.includes('loggedInUsers') && (
+					<NavLink to="/logout">Logout</NavLink>
+				)}
 			</nav>
 
 			<Routes>
 				<Route path="/welcome" element={<PageWelcome />} />
-				<Route path="/members" element={<PageMembers />} />
+				{currentUser.accessGroups.includes('members') && (
+					<Route path="/members" element={<PageMembers />} />
+				)}
 				<Route path="/register" element={<PageRegister />} />
 				<Route path="/login" element={<PageLogin />} />
-				<Route path="/logout" element={<PageLogout />} />
+				{currentUser.accessGroups.includes('loggedInUsers') && (
+					<Route path="/logout" element={<PageLogout />} />
+				)}
 				<Route path="/confirm-link" element={<PageConfirmLink />} />
 				<Route path="/" element={<Navigate to="/welcome" replace />} />
 			</Routes>
